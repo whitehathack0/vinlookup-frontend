a49a9fe611b58b8c4485c1904a524e958ab04655
whitehathack0 <adkumar9@gmail.com>
2022-05-24 21:30:56 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
+
+
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [vin, setVin] = useState("");
+  const [message, setMessage] = useState("");
+  
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      let res = await fetch("http://localhost:8000/api/vin/", {
+        method: 'POST',
+        body: JSON.stringify({ name: name, vin: vin }),
+        headers: { 
+          'Content-Type': 'application/json',
+        },
+      });
+
+      let resJson = await res.json();
+
+      if (res.status) {
+        setName("");
+        setVin("");
+        setMessage(resJson["data"]["year"] + " " + resJson["data"]["make"] + " " + resJson["data"]["model"] + " " + resJson["data"]["trim"]);
+      } 
+      else {
+        setMessage("Some error occured");
+      }
+
+    } catch (err) {
+      console.log("ERROR" + err);
+    }
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <><div className="title">
+      <h1> Vin lookup </h1>
     </div>
+
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)} />
+        <input
+          type="text"
+          value={vin}
+          placeholder="Vin"
+          onChange={(e) => setVin(e.target.value)} />
+
+        <button type="submit">Create</button>
+
+        <div className="message">{message ? <p>{message}</p> : null}
+        
+        
+        </div>
+      </form>
+    </div></>
   );
 }
 
